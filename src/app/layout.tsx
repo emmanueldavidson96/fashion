5e50974eec6c8bef99055814da05c2b71f3653a6
Emmanuel <emmanueldavidson524@gmail.com>
2025-03-03 11:04:04 +0100

    Fonts Have been changed to anton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Anton } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 
-const montserrat = Montserrat({subsets:["latin"]});
+const anton = Anton({weight:"400", subsets:["latin"]});
 
 export const metadata: Metadata = {
   title: "African Fashion Week Nigeria",
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${montserrat.className} bg-black antialiased overflow-x-hidden`}
+        className={`${anton.className} bg-black antialiased overflow-x-hidden`}
       >
         {children}
         <ToastContainer 
